2e60974545013d7a689044e382700772b8a39670
ba1chev <baychevyoan@gmail.com>
2026-09-01 15:45:48 +0300

    Add exercise_14: subtract two numbers

---
+++ Introduction-To-Programming/Exercises/Week-02/exercise_14.cpp
@@ -0,0 +1,9 @@
+// Да се въведат две числа - а и b. Да се изведе тяхната разлика.
+#include <iostream>
+
+int main() {
+    int a, b = 0;
+    std::cin >> a >> b;
+    std::cout << a - b << std::endl;
+    return 0;
+}
