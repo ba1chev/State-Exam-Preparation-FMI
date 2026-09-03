a47a63af3ff451f78f9f8760fe4d83d7e0a4b2ea
ba1chev <baychevyoan@gmail.com>
2026-09-03 10:43:32 +0300

    Add exercise_03_27: hack NASA progress simulation

---
+++ Introduction-To-Programming/Exercises/Week-03/exercise_27.cpp
@@ -0,0 +1,13 @@
+// Напишете програма, която да хакне NASA. 
+// Hint: нека поне изглежда по следния начин.
+#include <iostream>
+
+int main() {
+    std::cout << "Start hacking NASA" << std::endl;
+    for (size_t i = 0; i < 100; i++) {
+        std::cout << "Progress: " << i << "%" << std::endl;
+    }
+    std::cout << "Nasa hacked completely!!!" << std::endl;
+    return 0;
+}
+
