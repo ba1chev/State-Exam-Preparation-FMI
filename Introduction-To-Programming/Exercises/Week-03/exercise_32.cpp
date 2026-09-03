e544a4b4841c68edce9c2b5baf9e06a48833c0f6
ba1chev <baychevyoan@gmail.com>
2026-09-03 10:43:48 +0300

    Add exercise_03_32: print multiplication table for 7

---
+++ Introduction-To-Programming/Exercises/Week-03/exercise_32.cpp
@@ -0,0 +1,9 @@
+// Отпечатайте в стълб таблицата за умножение със 7.
+#include <iostream>
+
+int main() {
+    for (size_t i = 1; i <= 10; i++) {
+        std::cout << "7 * " << i << " = " << 7 * i << std::endl;
+    }
+    return 0;
+}
