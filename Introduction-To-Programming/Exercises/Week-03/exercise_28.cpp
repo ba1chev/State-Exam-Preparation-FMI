6ac9269590e92973d2a728a64399f3a51fec4d8e
ba1chev <baychevyoan@gmail.com>
2026-09-03 10:43:38 +0300

    Add exercise_03_28: print numbers 1 to 2 with step 0.1

---
+++ Introduction-To-Programming/Exercises/Week-03/exercise_28.cpp
@@ -0,0 +1,10 @@
+// Напишете програма, която 
+// изрежда числата между 1 и 2 със стъпка 0.1
+#include <iostream>
+
+int main() {
+    for (float i = 1; i < 2; i += 0.1) {
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
