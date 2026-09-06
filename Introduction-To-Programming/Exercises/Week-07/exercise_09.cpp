66d157dd53953cb33119d48adb104cff0792f1f3
ba1chev <baychevyoan@gmail.com>
2026-09-07 00:16:34 +0300

    Add exercise_07_09: find product of elements outside secondary diagonal

---
+++ Introduction-To-Programming/Exercises/Week-07/exercise_09.cpp
@@ -0,0 +1,50 @@
+// Дадена е квадратна реална матрица А с размерност kxk . 
+// Да се състави програма, която намира произведението от елементите 
+// извън вторичния главен диагонал.
+#include <iostream>
+
+void find_sum_without(const int* const* array, int k) {
+    if (!array || k < 0) {
+        std::cout << "Invalid function input" << std::endl;
+        return;
+    }
+
+    int result_sum = 1;
+    for (size_t i = 0; i < k; i++) {
+        for (size_t j = 0; j < k; j++) {
+            if ((i + j) == k - 1) {
+                continue;
+            }
+            result_sum *= array[i][j];
+        }
+    }
+    std::cout << result_sum << std::endl;
+}
+
+int main() {
+    int k = 0;
+    std::cin >> k;
+    if (k < 0) {
+        std::cout << "Invalid input" << std::endl;
+        return 1;
+    }
+
+    int** array = new int*[k]{};
+    for (size_t i = 0; i < k; i++) {
+        array[i] = new int[k]{};
+    }
+    for (size_t i = 0; i < k; i++) {
+        for (size_t j = 0; j < k; j++) {
+            std::cin >> array[i][j];
+        }
+    }
+    find_sum_without(array, k);
+
+    for (size_t i = 0; i < k; i++) {
+        delete[] array[i];
+        array[i] = nullptr;
+    }
+    delete[] array;
+    array = nullptr;
+    return 0;
+}
