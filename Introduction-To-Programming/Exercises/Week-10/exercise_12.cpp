923a7faf9e9fcbe57d1fbae0d1cb640d37595e0c
ba1chev <baychevyoan@gmail.com>
2026-09-10 10:55:42 +0300

    Add exercise_12: checked whether a matrix is symmetric about its main diagonal

---
+++ Introduction-To-Programming/Exercises/Week-10/exercise_12.cpp
@@ -0,0 +1,52 @@
+// Да се състави програма, която установява дали масива е симетричен относно главния си диагонал.
+#include <iostream>
+
+bool is_symetric(const int* const* array, int N, int M) {
+    if (!array || N < 0 || M < 0 || N != M) {
+        std::cout << "Invalid function input" << std::endl;
+        return false;
+    }
+
+    for (size_t i = 0; i < N; i++) {
+        if (!array[i]) {
+            std::cout << "Invalid function input" << std::endl;
+            return false;
+        }
+
+        for (size_t j = 0; j < M; j++) {
+            if (array[i][j] != array[j][i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    int N, M = 0;
+    std::cin >> N >> M;
+    if (N < 0 || M < 0) {
+        std::cout << "Invalid input" << std::endl;
+        return 1;
+    }
+
+    int** array = new int*[N]{nullptr};
+    for (size_t i = 0; i < N; i++) {
+        array[i] = new int[M]{};
+    }
+
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < M; j++) {
+            std::cin >> array[i][j];
+        }
+    }
+    std::cout << is_symetric(array, N, M) << std::endl;
+
+    for (size_t i = 0; i < N; i++) {
+        delete[] array[i];
+        array[i] = nullptr;
+    }
+    delete[] array;
+    array = nullptr;
+    return 0;
+}
