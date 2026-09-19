f04024ff3c46896a3d3bd8d5ceef33bfb07c40b2
ba1chev <baychevyoan@gmail.com>
2026-09-19 15:17:17 +0300

    Updated

--- Object-Oriented-Programming/Exercises/Week-06/exercise_03_main.cpp
+++ Object-Oriented-Programming/Exercises/Week-06/exercise_03_main.cpp
@@ -5,19 +5,19 @@ int main() {
     poly.setCoeffAtCurrentDegree(0, 1);
     poly.setCoeffAtCurrentDegree(1, 2);
     poly.setCoeffAtCurrentDegree(2, 3);
-    std::cout << poly << std::endl; // 1+(2)*x^1+(3)*x^2
+    std::cout << poly << std::endl;
 
-    std::cout << poly.getCoeffAtCurrentDegree(2) << std::endl; // 3
-    std::cout << poly.evaluate(2) << std::endl; // 17
+    std::cout << poly.getCoeffAtCurrentDegree(2) << std::endl;
+    std::cout << poly.evaluate(2) << std::endl;
 
     Polynomial copy = poly;
     copy.setCoeffAtCurrentDegree(0, 100);
-    std::cout << copy << std::endl; // 100+(2)*x^1+(3)*x^2
-    std::cout << poly << std::endl; // 1+(2)*x^1+(3)*x^2 (unchanged)
+    std::cout << copy << std::endl;
+    std::cout << poly << std::endl;
 
     Polynomial constant = Polynomial::createConstant(5);
-    std::cout << constant << std::endl; // 5
-    std::cout << constant.evaluate(42) << std::endl; // 5
+    std::cout << constant << std::endl;
+    std::cout << constant.evaluate(42) << std::endl;
 
     return 0;
 }
