d6bd1c7f1dfeef8128ffaa408988332cf5f51cb5
ba1chev <baychevyoan@gmail.com>
2026-09-19 15:43:24 +0300

    Updated

--- Object-Oriented-Programming/Exercises/Week-06/exercise_04_main.cpp
+++ Object-Oriented-Programming/Exercises/Week-06/exercise_04_main.cpp
@@ -3,21 +3,21 @@
 int main() {
     NumberSeries series(1, [](const int prev){ return prev + 2; });
 
-    std::cout << series.generateIElement(0) << std::endl; // 1
-    std::cout << series.generateIElement(4) << std::endl; // 9
-    std::cout << series.generateIElement(2) << std::endl; // 5 (cached)
+    std::cout << series.generateIElement(0) << std::endl;
+    std::cout << series.generateIElement(4) << std::endl;
+    std::cout << series.generateIElement(2) << std::endl;
 
-    std::cout << series.hasElement(7) << std::endl; // 1
-    std::cout << series.hasElement(8) << std::endl; // 0
+    std::cout << series.hasElement(7) << std::endl;
+    std::cout << series.hasElement(8) << std::endl;
 
     NumberSeries copy = series;
-    std::cout << copy.generateIElement(4) << std::endl; // 9 (independent copy)
+    std::cout << copy.generateIElement(4) << std::endl;
 
     series.setSeriesPredicate([](const int prev){ return prev * 2; });
-    std::cout << series.generateIElement(4) << std::endl; // 16 (1,2,4,8,16)
+    std::cout << series.generateIElement(4) << std::endl;
 
     series.setFirstElement(10);
-    std::cout << series.generateIElement(3) << std::endl; // 80 (10,20,40,80)
+    std::cout << series.generateIElement(3) << std::endl;
 
     return 0;
 }
