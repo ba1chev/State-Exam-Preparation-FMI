6a26a3005c89aa51c8c9ddcd95c5344061d8a720
ba1chev <baychevyoan@gmail.com>
2026-09-25 14:37:48 +0300

    Add Week-11 exercise_01 Student: derive from Human with faculty number and printMethod override

---
+++ Object-Oriented-Programming/Exercises/Week-11/exercise_01_student.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "exercise_01_human.h"
+
+class Student: public Human {
+private:
+    char* fnId = nullptr;
+
+    void free();
+    void moveTo(Student&& other) noexcept;
+    void copyFrom(const Student& other);
+
+public:
+    Student() = default;
+    Student(const char* name, const char* fdId, const size_t age);
+    Student(const Student& other);
+    Student(Student&& other) noexcept;
+    Student& operator = (const Student& other);
+    Student& operator = (Student&& other) noexcept;
+    ~Student();
+
+    void printMethod() const override;
+    const char* getFnId() const;
+};
